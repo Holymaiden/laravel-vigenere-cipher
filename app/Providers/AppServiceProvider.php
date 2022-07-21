41c725c4843c5603999621ccde7bb18c8c110d18
Holymaiden <haikalwatampone3@gmail.com>
2022-07-21 18:24:51 +0800

    initial

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        DB::listen(function ($query) {
+            Log::info(
+                $query->sql,
+                $query->bindings,
+                $query->time
+            );
+        });
+
+        $models = array(
+            'Role',
+            'User',
+        );
+
+        foreach ($models as $model) {
+            $this->app->bind("App\Services\Contracts\\{$model}Contract", "App\Services\\{$model}Service");
+        }
+    }
+}
